0aa1df5f94e57806d6f5734e220aa556e3ac5cbc
Yu-chen Chen <s9973938@gmail.com>
2024-07-29 22:54:10 +0800

    feat: add video [template]

---
+++ js/videoCarousel.js
@@ -0,0 +1,70 @@
+    
+$(document).ready(function() {
+    var $mainCarousel = $(".video-carousel").first();
+    var $modalCarousel = $(".modal-carousel");
+    var pressTimer;
+    var currentSlide = 0;
+  
+    // Initialize main carousel
+    $mainCarousel.owlCarousel({
+      items: 1,
+      dots: true,
+      mouseDrag: false,  // Disable mouse dragging
+      touchDrag: false,   // Disable touch dragging
+      dotsClass: 'video-dots'
+    });
+  
+
+    // Initialize modal carousel only once
+    var modalCarouselInitialized = false;
+
+    $('#videoCarouselModal').on('shown.bs.modal', function () {
+      if (!modalCarouselInitialized) {
+        $(".modal-carousel").owlCarousel({
+          items: 1,
+          dots: true,
+          dotsClass: 'video-dots'
+        });
+        modalCarouselInitialized = true;
+      }
+    });
+
+    function syncCarousels() {
+      $mainCarousel.on('changed.owl.carousel', function(event) {
+        currentSlide = event.item.index;
+        console.log('mainCarousel', currentSlide)
+        $modalCarousel.trigger('to.owl.carousel', [currentSlide, 0]);
+      });
+  
+      $modalCarousel.on('changed.owl.carousel', function(event) {
+        currentSlide = event.item.index;
+        console.log('modalCarousel', currentSlide)
+        $mainCarousel.trigger('to.owl.carousel', [currentSlide, 0]);
+      });
+    }
+
+    // On item click, open modal and go to specific slide
+    $('.item').on('mousedown touchstart', function(e) {
+      // Start tracking the press
+      pressTimer = setTimeout(function() {
+        // Do nothing on long press
+      }, 500); // Adjust the long press duration
+    }).on('mouseup touchend', function(e) {
+      clearTimeout(pressTimer);
+      var slideTo = $(this).data('slide-to');
+      var carouselModal = new bootstrap.Modal(document.getElementById('videoCarouselModal'));
+      carouselModal.show();
+  
+      $('#videoCarouselModal').on('shown.bs.modal', function () {
+        $modalCarousel.trigger('to.owl.carousel', [slideTo, 0]); // Jump to the startPosition without animation; [slideTo, 300]
+      });
+
+      syncCarousels();
+    });
+  
+    // Reset modal carousel position when closed
+    // $('#videoCarouselModal').on('hidden.bs.modal', function() {
+    //   $modalCarousel.trigger('to.owl.carousel', [0, 0]);
+    // });
+  });
+  
