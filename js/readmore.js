5c11f69272aeede25bea70531e3f594aff68bd2f
effie <smalldow0@gmail.com>
2024-08-02 04:28:01 +0800

    fix: readmore

--- js/readmore.js
+++ js/readmore.js
@@ -1,49 +1,66 @@
-// define the card height when it is not expanded
-
 const cardHeight = 160;
+const certificateHeight = 212;
 
-// get all cards
 const cards = document.querySelectorAll(".description");
 
-// loop through cards
 for (let card of cards) {
-  const description = card.querySelector(".description-content");
-  const readMore = card.querySelector(".description-button");
 
-  // limit description height to s200px
-  description.style.maxHeight = `${cardHeight}px`;
-  // hide read more button if description is less than card height
-  if (description.scrollHeight <= cardHeight) {
-    readMore.style.display = "none";
+  const description = card.querySelector(".description-content");
+  const certificate = card.querySelector(".description-content.certificate");
+  const readMore = card.querySelector(".description-button");
+  const readMoreNoDot = card.querySelector(".description-button-nodot");
+
+  if (description != null) {
+    description.style.maxHeight = `${cardHeight}px`;
+    if (description.scrollHeight <= cardHeight && readMore) {
+      readMore.style.display = "none";
+    }  
+  }
+  if (certificate != null) {
+    certificate.style.maxHeight = `${certificateHeight}px`;
+    if (certificate.scrollHeight <= certificateHeight &&readMoreNoDot){
+      readMoreNoDot.style.display = "none";
+    }
   }
 
-  // function to add span with hellip
   const addSpan = () => {
     let span = document.createElement("span");
     span.innerHTML = "&hellip;";
-    // append span after description
     description.parentNode.insertBefore(span, description.nextSibling);
   };
 
-  // add a span with hellip at the end of the container if the card is not expanded
-  if (description.scrollHeight > cardHeight) {
+  if (description.scrollHeight > cardHeight && !readMoreNoDot) {
     addSpan();
-  }
+  } 
 
-  // add event listener to read more button to toggle description height
   if (readMore) {
-    readMore.addEventListener("click", () => {
-      if (description.style.maxHeight === `${cardHeight}px`) {
-        description.style.maxHeight = `${description.scrollHeight}px`;
-        readMore.innerHTML = "顯示較少內容";
-        // remove span with hellip when description is expanded
-        description.nextSibling.remove();
+      readMore.addEventListener("click", () => {
+        if (description.style.maxHeight === `${cardHeight}px`) {
+          description.style.maxHeight = `${description.scrollHeight}px`;
+          const arrow_up =`<i class="fa-solid fa-angle-up"></i>`;
+          readMore.innerHTML = "顯示較少內容"+arrow_up;
+          // remove span with hellip when description is expanded
+          description.nextSibling.remove();
+        } else {
+          description.style.maxHeight = `${cardHeight}px`;
+          const arrow_down =`<i class="fa-solid fa-angle-down"></i>`;
+          readMore.innerHTML = "顯示更多內容"+arrow_down;
+          // add span with hellip when description is collapsed
+          addSpan();
+        }
+      });
+  }
+  if (readMoreNoDot) {
+    readMoreNoDot.addEventListener("click", () => {
+      if (certificate.style.maxHeight === `${certificateHeight}px`) {
+        certificate.style.maxHeight = `${certificate.scrollHeight}px`;
+        const arrow_up =`<i class="fa-solid fa-angle-up"></i>`;
+        readMoreNoDot.innerHTML = "顯示較少內容"+arrow_up;
       } else {
-        description.style.maxHeight = `${cardHeight}px`;
-        readMore.innerHTML = "顯示更多內容";
-        // add span with hellip when description is collapsed
-        addSpan();
+        certificate.style.maxHeight = `${certificateHeight}px`;
+        const arrow_down =`<i class="fa-solid fa-angle-down"></i>`;
+        readMoreNoDot.innerHTML = "顯示更多內容"+arrow_down;
       }
-    });
+    })
   }
 }
